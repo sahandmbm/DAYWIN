39b01733ca4f3c5d3a44a281b68c4fe2aa68d674
Sahand Shahriari <42390009+sahandmbm@users.noreply.github.com>
2018-08-30 22:04:18 +0100

    Add files via upload

---
+++ ABOUT/JAVASCRIPT/JavaScript.js
@@ -0,0 +1,44 @@
+
+//nav--------------------------------------------------
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
+
+
+//PICS-----------------------------------------------
+$('.show1').show();
+$(".hide1").hide();
+
+$("#buttonShow").click(function(){
+        $(".hide1").fadeIn(3000);
+        $("#buttonShow").fadeOut();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
